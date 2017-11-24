4f17b9757190d9bf1eb257a2486c2f72a936f41a
Mike Resoli <mike.resoli@remarkable.net>
2017-11-24 11:11:37 +0000

    init

---
+++ RussiaRegexTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RussiaRegexTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Use this regex to allow French, German, etc special chars: [^A-Za-z0-9\x80-\xFF-]
+            string russian = "Черные штаны прямого покроя с эластичным поясом - PETITE Размеры Плюс от 16 до 32";
+            string english = "Black Classic Straight Leg Trousers With Elasticated Waistband - PETITE";
+
+            russian = russian.Trim().Replace(" ", "-");
+            english = english.Trim().Replace(" ", "-");
+
+            russian = "/" + Regex.Replace(russian, "[^A-Za-z0-9\x80-\xFF-\u0400-\u04FF]", "");
+            while (russian.IndexOf("--") > -1) russian = russian.Replace("--", "-");
+
+            english = "/" + Regex.Replace(english, "[^A-Za-z0-9\x80-\xFF-]", "");
+            while (english.IndexOf("--") > -1) english = english.Replace("--", "-");
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine(russian + "\n" + english + "\nPress any key to continue");
+            Console.ReadKey();
+        }
+    }
+}
